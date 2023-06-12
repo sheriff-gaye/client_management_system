32e95b550dcc3b911352299fd9f77bf7c2466c0b
sheriff-gaye <sheriffgaye5@gmail.com>
2023-06-12 22:26:19 +0000

    all

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -84,9 +84,13 @@
 
             @endif
             <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200" href="{{route('chatify')}}">
-              <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
+              <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M19 21H5a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2z" />
+                <polyline points="17 8 12 13 7 8" />
               </svg>
+
+
+
               <span class="ml-4 ">Chat</span>
             </a>
           </li>
